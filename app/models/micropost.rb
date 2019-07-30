5aabf72615b14ae02081fcc44dcd9770a03a7222
IzumiShiozaki <over.drive.0108@outlook.jp>
2019-07-30 16:58:41 +0900

    Add user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,17 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> {order(created_at: :desc)} #default_scopeでマイクロポストを順序づけする
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
+  validate :picture_size # 画像に対するバリデーションを追加する
+
+  private
+  
+    #アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
